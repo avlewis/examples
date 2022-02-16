2659c8961e219f68ea65601a6beb2a996de91183
austin <austinvlewis@gmail.com>
2022-02-16 10:22:54 -0600

    updates

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -16,7 +16,7 @@ export default function AppRouter(){
     return(
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home />} exact />
                 {examplesRoutes.map((r) => 
                     <Route 
                         path={r.path}
